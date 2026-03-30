9b3c876ab41480a4e4f7dbc461fd3c0449fde690
dmunarg2023 <d.munarg@uniandes.edu.co>
2026-03-30 17:00:29 -0500

    publisher_TCP

--- TCP/publisher_tcp.c
+++ TCP/publisher_tcp.c
@@ -5,60 +5,36 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1"
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
+    int sock;
+    struct sockaddr_in server_addr;
+    char message[1024];
 
-    // 1. Crear socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("Error al crear socket");
-        exit(EXIT_FAILURE);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Error creando socket");
+        return 1;
     }
 
-    // 2. Configurar dirección
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // acepta cualquier IP
-    address.sin_port = htons(PORT);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
 
-    // 3. Bind
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Error en bind");
-        exit(EXIT_FAILURE);
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error conectando al broker");
+        return 1;
     }
 
-    // 4. Escuchar
-    if (listen(server_fd, 5) < 0) {
-        perror("Error en listen");
-        exit(EXIT_FAILURE);
-    }
+    printf("Conectado al broker TCP...\n");
 
-    printf("Broker escuchando en el puerto %d...\n", PORT);
-
-    // 5. Loop para aceptar clientes
     while (1) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        if (new_socket < 0) {
-            perror("Error en accept");
-            continue;
-        }
-
-        printf("Cliente conectado!\n");
-
-        // 6. Recibir mensajes
-        int valread;
-        while ((valread = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-            buffer[valread] = '\0';
-            printf("Mensaje recibido: %s\n", buffer);
-        }
-
-        printf("Cliente desconectado\n");
-        close(new_socket);
+        printf("Ingrese mensaje: ");
+        fgets(message, sizeof(message), stdin);
+        send(sock, message, strlen(message), 0);
     }
 
+    close(sock);
     return 0;
 }
